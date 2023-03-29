d2d2656705daff21b7cce473586e310bbe44b6b2
Nata R <natasha.melanis@yandex.ru>
2023-03-29 23:42:05 +0300

    BonusMilesService changed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketCost = 12_000;
-        int oneMile = 20;
-        int bonus = ticketCost / oneMile;
+        int price = 10_000;
+        int miles = 20;
+        int bonus = price / miles;
 
-        System.out.println (bonus);
+        System.out.println(bonus);
     }
 }
