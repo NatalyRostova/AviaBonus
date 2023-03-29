24c29a883305ef756dc7a3f25299b15cebfa2098
Nata R <natasha.melanis@yandex.ru>
2023-03-29 23:43:04 +0300

    Revert "BonusMilesService changed"
    This reverts commit d2d2656705daff21b7cce473586e310bbe44b6b2.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 10_000;
-        int miles = 20;
-        int bonus = price / miles;
+        int ticketCost = 12_000;
+        int oneMile = 20;
+        int bonus = ticketCost / oneMile;
 
-        System.out.println(bonus);
+        System.out.println (bonus);
     }
 }
